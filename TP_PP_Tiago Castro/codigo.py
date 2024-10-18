aaa522942a06a30e724a73a3c37ff35c559e45f1
tiagodecastro <tiagocastro1994@gmail.com>
2024-10-18 16:26:59 +0100

    add Projeto Final

---
+++ TP_PP_Tiago Castro/codigo.py
@@ -0,0 +1,12 @@
+print("*** Menu de Login ***\n")
+print("Escolha o seu cargo de login: ")
+print("1. Administrador")
+print("2. Engenheiro de Manutenções")
+print("3. Cliente")
+
+if
+    login = input("Login: ")
+    password = input("Password: ")
+
+    if login == "admin" and password == 'cesae123':
+        print("*** Bem Vindo ***")
